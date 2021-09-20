0d49eb2e9df89a294e07373ba3d58bcc9abea2d3
Noah Zinsmeister <noahwz@gmail.com>
2021-09-20 16:29:16 -0400

    add checkPreviousBlockhash

--- contracts/base/PeripheryValidation.sol
+++ contracts/base/PeripheryValidation.sol
@@ -1,10 +1,15 @@
 // SPDX-License-Identifier: GPL-2.0-or-later
 pragma solidity =0.7.6;
 
+import '../interfaces/IPeripheryValidation.sol';
 import './BlockTimestamp.sol';
 
-abstract contract PeripheryValidation is BlockTimestamp {
-    function checkDeadline(uint256 deadline) external view {
+abstract contract PeripheryValidation is IPeripheryValidation, BlockTimestamp {
+    function checkDeadline(uint256 deadline) external override view {
         require(_blockTimestamp() <= deadline, 'Transaction too old');
+    }
+
+    function checkPreviousBlockhash(bytes32 previousBlockhash) external override view {
+        require(blockhash(block.number - 1) == previousBlockhash, 'Unexpected parent block');
     }
 }
