4684655c5b62506f3cab9966b6f6ab5a0ab87cdb
Noah Zinsmeister <noahwz@gmail.com>
2021-09-20 17:09:23 -0400

    add skeleton V2SwapRouter

---
+++ contracts/interfaces/IV2SwapRouter.sol
@@ -0,0 +1,7 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
+pragma solidity >=0.7.5;
+pragma abicoder v2;
+
+/// @title Router token swapping functionality
+/// @notice Functions for swapping tokens via Uniswap V2
+interface IV2SwapRouter {}
