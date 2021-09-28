51f88a46a79c937e9814667d82b50b909e16c88c
Noah Zinsmeister <noahwz@gmail.com>
2021-09-28 14:21:31 -0400

    add more interfaces to ISwapRouter02

--- contracts/interfaces/ISwapRouter02.sol
+++ contracts/interfaces/ISwapRouter02.sol
@@ -2,10 +2,14 @@
 pragma solidity >=0.7.5;
 pragma abicoder v2;
 
+import '@uniswap/v3-periphery/contracts/interfaces/ISelfPermit.sol';
+
 import './IV2SwapRouter.sol';
 import './IV3SwapRouter.sol';
+import './IApproveAndCall.sol';
+import './IMulticallExtended.sol';
 
 /// @title Router token swapping functionality
-interface ISwapRouter02 is IV2SwapRouter, IV3SwapRouter {
+interface ISwapRouter02 is IV2SwapRouter, IV3SwapRouter, IApproveAndCall, IMulticallExtended, ISelfPermit {
 
 }
