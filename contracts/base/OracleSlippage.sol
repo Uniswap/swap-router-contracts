0164bcdf3a56f79dfcfc9e485ebe3c0727f5d26f
Noah Zinsmeister <noahwz@gmail.com>
2021-11-03 11:51:39 -0400

    refactor; support secondsAgo

--- contracts/base/OracleSlippage.sol
+++ contracts/base/OracleSlippage.sol
@@ -6,6 +6,8 @@ import '@uniswap/v3-periphery/contracts/libraries/Path.sol';
 import '@uniswap/v3-periphery/contracts/libraries/PoolAddress.sol';
 import '@uniswap/v3-core/contracts/interfaces/IUniswapV3Pool.sol';
 
+import '../libraries/OracleLibrary.sol';
+
 abstract contract OracleSlippage is PeripheryImmutableState {
     using Path for bytes;
 
@@ -37,48 +39,76 @@ abstract contract OracleSlippage is PeripheryImmutableState {
         return (blockStartingTick, currentTick);
     }
 
-    function oracleSlippage(bytes memory path, uint24 maximumTickDivergence) external view {
-        require(path.numPools() > 0, 'Path must be valid');
-
+    function oracleSlippage(
+        bytes memory path,
+        uint24 maximumTickDivergence,
+        uint32 secondsAgo
+    ) external view {
         // running synthetic ticks
-        int24 syntheticBlockStartingTick;
-        int24 syntheticCurrentTick;
+        int256 syntheticAverageTick;
+        int256 syntheticCurrentTick;
 
+        // this assumes the path is sorted in swap order
         (address tokenIn, address tokenOut, uint24 fee) = path.decodeFirstPool();
+        // if tokenIn < tokenOut, then tokenIn is token0 of the first pool,
+        // meaning the tick represents an x/tokenIn price.
+        // so, the lower the x/tokenIn price gets, the worse of a price the swap will get
         bool lowerTicksAreWorse = tokenIn < tokenOut;
+
+        // get the address of the first pool
+        address pool = PoolAddress.computeAddress(factory, PoolAddress.getPoolKey(tokenIn, tokenOut, fee));
+
+        // we optimize for the secondsAgo == 0 case, i.e. since the beginning of the block
+        if (secondsAgo == 0) {
+            (syntheticAverageTick, syntheticCurrentTick) = getBlockStartingAndCurrentTick(pool);
+        } else {
+            (syntheticAverageTick, ) = OracleLibrary.consult(pool, secondsAgo);
+            (, syntheticCurrentTick, , , , , ) = IUniswapV3Pool(pool).slot0();
+        }
+
+        // address tokenOutNext;
+        // if the path includes > 1 pool, then chain the prices
+        path = path.skipToken();
         address tokenOutNext;
-
+        int24 averageTick;
+        int24 currentTick;
         while (path.numPools() > 0) {
-            address pool = PoolAddress.computeAddress(factory, PoolAddress.getPoolKey(tokenIn, tokenOut, fee));
-            (int24 blockStartingTick, int24 currentTick) = getBlockStartingAndCurrentTick(pool);
-
-            path = path.skipToken();
             (, tokenOutNext, fee) = path.decodeFirstPool();
+            pool = PoolAddress.computeAddress(factory, PoolAddress.getPoolKey(tokenOut, tokenOutNext, fee));
+
+            // get the average and current ticks in the current pool
+            if (secondsAgo == 0) {
+                (averageTick, currentTick) = getBlockStartingAndCurrentTick(pool);
+            } else {
+                (averageTick, ) = OracleLibrary.consult(pool, secondsAgo);
+                (, currentTick, , , , , ) = IUniswapV3Pool(pool).slot0();
+            }
 
             // chain ticks together
             bool add = tokenIn < tokenOut ? tokenOut < tokenOutNext : tokenOutNext < tokenIn;
             if (add) {
-                syntheticBlockStartingTick += blockStartingTick;
+                syntheticAverageTick += averageTick;
                 syntheticCurrentTick += currentTick;
             } else {
-                syntheticBlockStartingTick -= blockStartingTick;
+                syntheticAverageTick -= averageTick;
                 syntheticCurrentTick -= currentTick;
             }
 
             tokenIn = tokenOut;
             tokenOut = tokenOutNext;
+            path = path.skipToken();
         }
 
         if (lowerTicksAreWorse) {
             require(
-                syntheticCurrentTick >= syntheticBlockStartingTick ||
-                    uint24(syntheticBlockStartingTick - syntheticCurrentTick) < maximumTickDivergence,
+                syntheticCurrentTick >= syntheticAverageTick ||
+                    syntheticAverageTick - syntheticCurrentTick < maximumTickDivergence,
                 'Divergence'
             );
         } else {
             require(
-                syntheticCurrentTick <= syntheticBlockStartingTick ||
-                    uint24(syntheticCurrentTick - syntheticBlockStartingTick) < maximumTickDivergence,
+                syntheticCurrentTick <= syntheticAverageTick ||
+                    syntheticCurrentTick - syntheticAverageTick < maximumTickDivergence,
                 'Divergence'
             );
         }
