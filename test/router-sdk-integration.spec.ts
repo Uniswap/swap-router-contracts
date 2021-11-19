51174c9a1f93f5f493e44475f37f6811668002f0
Emily Williams <emag3m@gmail.com>
2021-11-18 19:19:56 -0500

    update tests

--- test/router-sdk-integration.spec.ts
+++ test/router-sdk-integration.spec.ts
@@ -31,7 +31,7 @@ import PositionValueJSON from '@uniswap/v3-periphery/artifacts/contracts/librari
 
 const toWei = (n: string) => utils.parseEther(n).toString()
 const ADDRESS_THIS = '0x0000000000000000000000000000000000000001'
-const TOKEN_LIQUIDITY_AMOUNT = toWei('1000000')
+const TOKEN_LIQUIDITY_AMOUNT = toWei('100')
 
 describe.only('router-sdk integration', function () {
   let wallet: Wallet
@@ -54,6 +54,10 @@ describe.only('router-sdk integration', function () {
       await token.connect(trader).approve(router.address, constants.MaxUint256)
       await token.transfer(trader.address, expandTo18Decimals(1_000_000))
     }
+
+		await weth9.deposit({ value:  TOKEN_LIQUIDITY_AMOUNT})
+		await weth9.approve(router.address, constants.MaxUint256)
+		await weth9.approve(nft.address, constants.MaxUint256)
 
     return {
       weth9,
@@ -137,7 +141,7 @@ describe.only('router-sdk integration', function () {
   })
 
   beforeEach('load fixture', async () => {
-    ;({ router, tokens, nft, factory } = await loadFixture(swapRouterFixture))
+    ;({ router, tokens, nft, factory, weth9 } = await loadFixture(swapRouterFixture))
 		const positionValueFactory = await ethers.getContractFactory('TestPositionValue')
 		positionValue = await positionValueFactory.deploy() as TestPositionValue
   })
@@ -146,24 +150,29 @@ describe.only('router-sdk integration', function () {
     let token0: Token
     let token1: Token
     let token2: Token
+    let weth9sdk: Token
 
     let pool_0_1: Pool
     let pool_1_2: Pool
     let pool_0_2: Pool
+    let pool_0_weth: Pool
 
 		let position: Position
     let methodParameters: MethodParameters
+    let amountInDesired: CurrencyAmount<Token>
 
     beforeEach('create on-chain contracts', async () => {
       await createPool(tokens[0].address, tokens[1].address)
       await createPool(tokens[1].address, tokens[2].address)
 			await createPool(tokens[0].address, tokens[2].address)
+			await createPool(weth9.address, tokens[2].address)
     })
 
     beforeEach('create sdk components', async () => {
       token0 = new Token(1, tokens[0].address, 18)
       token1 = new Token(1, tokens[1].address, 18)
       token2 = new Token(1, tokens[2].address, 18)
+      weth9sdk = new Token(1, weth9.address, 18)
 
       pool_0_1 = v2StylePool(
         CurrencyAmount.fromRawAmount(token0, TOKEN_LIQUIDITY_AMOUNT),
@@ -180,84 +189,121 @@ describe.only('router-sdk integration', function () {
         CurrencyAmount.fromRawAmount(token2, TOKEN_LIQUIDITY_AMOUNT)
       )
 
-      // setup trade components
-      const slippageTolerance = new Percent(1)
-      const routeOriginal = new V3RouteSDK([pool_0_1, pool_1_2], token0, token2)
-      const route = new RouteV3(routeOriginal)
-      const amount = CurrencyAmount.fromRawAmount(token0, toWei('1'))
-      const tradeType = TradeType.EXACT_INPUT
-      const expectedOut = await pool_0_1.getOutputAmount(amount)
-      const trade = await Trade.fromRoute(route, amount, tradeType)
-
-      // setup position components
-      position = Position.fromAmounts({
-				pool: pool_0_2,
-        tickLower: -60,
-        tickUpper: 60,
-				amount0: toWei('1'),
-				amount1: toWei('1'),
-				useFullPrecision: true
-			})
-      const addLiquidityOptions = {
-        recipient: wallet.address,
-        slippageTolerance,
-        deadline: 2 ** 32,
-      }
-
-      methodParameters = SwapRouter.swapAndAddCallParameters(
-        trade,
-        { slippageTolerance },
-        position,
-        addLiquidityOptions
-      )
-    })
-
-    it('returns encoded calldata', async () => {
-      expect(methodParameters.calldata).to.equal(
-        '0xac9650d8000000000000000000000000000000000000000000000000000000000000002000000000000000000000000000000000000000000000000000000000000000080000000000000000000000000000000000000000000000000000000000000100000000000000000000000000000000000000000000000000000000000000026000000000000000000000000000000000000000000000000000000000000002e0000000000000000000000000000000000000000000000000000000000000036000000000000000000000000000000000000000000000000000000000000003c00000000000000000000000000000000000000000000000000000000000000420000000000000000000000000000000000000000000000000000000000000062000000000000000000000000000000000000000000000000000000000000006a00000000000000000000000000000000000000000000000000000000000000124b858183f0000000000000000000000000000000000000000000000000000000000000020000000000000000000000000000000000000000000000000000000000000008000000000000000000000000000000000000000000000000000000000000000010000000000000000000000000000000000000000000000000de0b6b3a764000000000000000000000000000000000000000000000000000006e5b60f3d2e9d6600000000000000000000000000000000000000000000000000000000000000420165878a594ca255338adfa4d48449f69242eb8f000bb85fc8d32690cc91d4c39d9d3abcbd16989f875707000bb8a513e6e4b8f2a923d98304ec87f64353c4d5c853000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000044f2d5d56b000000000000000000000000a513e6e4b8f2a923d98304ec87f64353c4d5c85300000000000000000000000000000000000000000000000006fb00a46a35629a000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000044f2d5d56b0000000000000000000000000165878a594ca255338adfa4d48449f69242eb8f0000000000000000000000000000000000000000000000000de0b6b3a7640000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000024571ac8b00000000000000000000000000165878a594ca255338adfa4d48449f69242eb8f000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000024571ac8b0000000000000000000000000a513e6e4b8f2a923d98304ec87f64353c4d5c8530000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000001c4b3a2af1300000000000000000000000000000000000000000000000000000000000000200000000000000000000000000000000000000000000000000000000000000164883164560000000000000000000000000165878a594ca255338adfa4d48449f69242eb8f000000000000000000000000a513e6e4b8f2a923d98304ec87f64353c4d5c8530000000000000000000000000000000000000000000000000000000000000bb8ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffc4000000000000000000000000000000000000000000000000000000000000003c0000000000000000000000000000000000000000000000000de0b6b3a76400000000000000000000000000000000000000000000000000000de0b6b3a764000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000f39fd6e51aad88f6f4ce6ab8827279cfffb92266000000000000000000000000000000000000000000000000000000010000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000044e90a182f0000000000000000000000000165878a594ca255338adfa4d48449f69242eb8f0000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000044e90a182f000000000000000000000000a513e6e4b8f2a923d98304ec87f64353c4d5c853000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000'
-      )
-    })
-
-    it('reverts if tokens are not approved to the router', async () => {
-			await tokens[0].approve(router.address, 0)
-			await expect(router['multicall(bytes[])']([methodParameters.calldata], { value: 0 })).to.be.revertedWith('STF')
-		})
-
-
-    it('mints the correct position', async () => {
-			const [token_0, token_1] = tokens[0].address < tokens[2].address ? [tokens[0], tokens[2]] : [tokens[2], tokens[0]]
-			const poolAddress = await factory.getPool(tokens[0].address, tokens[2].address, FeeAmount.MEDIUM)
-			const tokenId = 4
-
-			await expect(nft.positions(tokenId)).to.be.revertedWith(
-				"VM Exception while processing transaction: revert Invalid token ID"
+			pool_0_weth = v2StylePool(
+				CurrencyAmount.fromRawAmount(token0, TOKEN_LIQUIDITY_AMOUNT),
+				CurrencyAmount.fromRawAmount(weth9sdk, TOKEN_LIQUIDITY_AMOUNT)
 			)
-			await router['multicall(bytes[])']([methodParameters.calldata], { value: 0 })
+    })
 
-			const mintedPosition = await nft.positions(tokenId)
-			expect(mintedPosition.tickLower).to.equal(position.tickLower)
-			expect(mintedPosition.tickUpper).to.equal(position.tickUpper)
-			expect(mintedPosition.token0).to.equal(token_0.address)
-			expect(mintedPosition.token1).to.equal(token_1.address)
-			expect(mintedPosition.liquidity.toString()).to.equal(position.liquidity.toString())
-			expect(await nft.ownerOf(tokenId)).to.equal(wallet.address)
-		})
+    describe('erc20 --> erc20', () => {
+      beforeEach(async () => {
+        // setup trade components
+        const slippageTolerance = new Percent(1)
+        const routeOriginal = new V3RouteSDK([pool_0_1, pool_1_2], token0, token2)
+        const route = new RouteV3(routeOriginal)
+        amountInDesired = CurrencyAmount.fromRawAmount(token0, toWei('1'))
+        const expectedOut = await pool_0_1.getOutputAmount(amountInDesired)
+        const trade = await Trade.fromRoute(route, amountInDesired, TradeType.EXACT_INPUT)
 
-		it('distributes token balances correctly', async () => {
-			const [token_0, token_1] = tokens[0].address < tokens[2].address ? [tokens[0], tokens[2]] : [tokens[2], tokens[0]]
-			const poolAddress = await factory.getPool(tokens[0].address, tokens[2].address, FeeAmount.MEDIUM)
-			const tokenId = 4
+        // setup position components
+        position = Position.fromAmounts({
+          pool: pool_0_2,
+          tickLower: -60,
+          tickUpper: 60,
+          amount0: toWei('1'),
+          amount1: toWei('1'),
+          useFullPrecision: true
+        })
+        const addLiquidityOptions = {
+          recipient: wallet.address,
+          slippageTolerance,
+          deadline: 2 ** 32,
+        }
 
-			const poolBalancePrev0 = await tokens[0].balanceOf(poolAddress)
-			const poolBalancePrev2 = await tokens[2].balanceOf(poolAddress)
+        methodParameters = SwapRouter.swapAndAddCallParameters(
+          trade,
+          { slippageTolerance },
+          position,
+          addLiquidityOptions
+        )
+      })
 
-			await router['multicall(bytes[])']([methodParameters.calldata], { value: 0 })
+      it.skip('returns encoded calldata', async () => {
+        expect(methodParameters.calldata).to.equal(
+          '0xac9650d8000000000000000000000000000000000000000000000000000000000000002000000000000000000000000000000000000000000000000000000000000000080000000000000000000000000000000000000000000000000000000000000100000000000000000000000000000000000000000000000000000000000000026000000000000000000000000000000000000000000000000000000000000002e0000000000000000000000000000000000000000000000000000000000000036000000000000000000000000000000000000000000000000000000000000003c00000000000000000000000000000000000000000000000000000000000000420000000000000000000000000000000000000000000000000000000000000062000000000000000000000000000000000000000000000000000000000000006a00000000000000000000000000000000000000000000000000000000000000124b858183f0000000000000000000000000000000000000000000000000000000000000020000000000000000000000000000000000000000000000000000000000000008000000000000000000000000000000000000000000000000000000000000000010000000000000000000000000000000000000000000000000de0b6b3a764000000000000000000000000000000000000000000000000000006c33ed4b59e17e400000000000000000000000000000000000000000000000000000000000000420165878a594ca255338adfa4d48449f69242eb8f000bb85fc8d32690cc91d4c39d9d3abcbd16989f875707000bb8a513e6e4b8f2a923d98304ec87f64353c4d5c853000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000044f2d5d56b000000000000000000000000a513e6e4b8f2a923d98304ec87f64353c4d5c853000000000000000000000000000000000000000000000000071d77def1c5e81c000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000044f2d5d56b0000000000000000000000000165878a594ca255338adfa4d48449f69242eb8f0000000000000000000000000000000000000000000000000de0b6b3a7640000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000024571ac8b00000000000000000000000000165878a594ca255338adfa4d48449f69242eb8f000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000024571ac8b0000000000000000000000000a513e6e4b8f2a923d98304ec87f64353c4d5c8530000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000001c4b3a2af1300000000000000000000000000000000000000000000000000000000000000200000000000000000000000000000000000000000000000000000000000000164883164560000000000000000000000000165878a594ca255338adfa4d48449f69242eb8f000000000000000000000000a513e6e4b8f2a923d98304ec87f64353c4d5c8530000000000000000000000000000000000000000000000000000000000000bb8ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffc4000000000000000000000000000000000000000000000000000000000000003c0000000000000000000000000000000000000000000000000de0b6b3a76400000000000000000000000000000000000000000000000000000de0b6b3a764000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000f39fd6e51aad88f6f4ce6ab8827279cfffb92266000000000000000000000000000000000000000000000000000000010000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000044e90a182f0000000000000000000000000165878a594ca255338adfa4d48449f69242eb8f0000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000044e90a182f000000000000000000000000a513e6e4b8f2a923d98304ec87f64353c4d5c853000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000'
+        )
+      })
 
-			const poolBalanceCurrent0 = await tokens[0].balanceOf(poolAddress)
-			const poolBalanceCurrent2= await tokens[2].balanceOf(poolAddress)
+      it('reverts if tokens are not approved to the router', async () => {
+  			await tokens[0].approve(router.address, 0)
+  			await expect(router['multicall(bytes[])']([methodParameters.calldata], { value: 0 })).to.be.revertedWith('STF')
+  		})
 
-			expect(poolBalanceCurrent0.sub(poolBalancePrev0)).to.equal(toWei('1'))
-			expect(poolBalanceCurrent2.sub(poolBalancePrev2)).to.equal(toWei('1'))
-		})
+
+      it('mints the correct position', async () => {
+  			const [token_0, token_1] = tokens[0].address < tokens[2].address ? [tokens[0], tokens[2]] : [tokens[2], tokens[0]]
+  			const poolAddress = await factory.getPool(tokens[0].address, tokens[2].address, FeeAmount.MEDIUM)
+  			const tokenId = 4
+
+  			await expect(nft.positions(tokenId)).to.be.revertedWith(
+  				"VM Exception while processing transaction: revert Invalid token ID"
+  			)
+  			await router['multicall(bytes[])']([methodParameters.calldata], { value: 0 })
+
+  			const mintedPosition = await nft.positions(tokenId)
+  			expect(mintedPosition.tickLower).to.equal(position.tickLower)
+  			expect(mintedPosition.tickUpper).to.equal(position.tickUpper)
+  			expect(mintedPosition.token0).to.equal(token_0.address)
+  			expect(mintedPosition.token1).to.equal(token_1.address)
+  			expect(mintedPosition.liquidity.toString()).to.equal(position.liquidity.toString())
+  			expect(await nft.ownerOf(tokenId)).to.equal(wallet.address)
+  		})
+
+  		it.only('distributes token balances correctly', async () => {
+  			const [token_0, token_1] = tokens[0].address < tokens[2].address ? [tokens[0], tokens[2]] : [tokens[2], tokens[0]]
+  			const poolAddress = await factory.getPool(tokens[0].address, tokens[2].address, FeeAmount.MEDIUM)
+  			const tokenId = 4
+
+        // track previous balances
+  			const poolBalancePrev0 = await tokens[0].balanceOf(poolAddress)
+  			const poolBalancePrev2 = await tokens[2].balanceOf(poolAddress)
+        const userBalancePrev0 = await tokens[0].balanceOf(wallet.address)
+        const userBalancePrev2 = await tokens[2].balanceOf(wallet.address)
+
+        const expectedOut_0_1 = (await pool_0_1.getOutputAmount(amountInDesired))[0]
+        const amount2FromSwap = (await pool_1_2.getOutputAmount(expectedOut_0_1))[0]
+  			const tx = await (await router['multicall(bytes[])']([methodParameters.calldata], { value: 0 })).wait()
+
+        // track ending balances
+  			const poolBalanceCurrent0 = await tokens[0].balanceOf(poolAddress)
+  			const poolBalanceCurrent2= await tokens[2].balanceOf(poolAddress)
+        const userBalanceCurrent0 = await tokens[0].balanceOf(wallet.address)
+        const userBalanceCurrent2 = await tokens[2].balanceOf(wallet.address)
+
+        // test balances are correct
+  			expect(poolBalanceCurrent0.sub(poolBalancePrev0)).to.equal(amountInDesired.quotient.toString())
+  			expect(poolBalanceCurrent2.sub(poolBalancePrev2)).to.equal(amountInDesired.quotient.toString())
+        expect(await tokens[0].balanceOf(router.address)).to.equal(0)
+        expect(await tokens[2].balanceOf(router.address)).to.equal(0)
+        expect(userBalancePrev0.sub(userBalanceCurrent0)).to.equal(toWei('2'))
+
+        // test correct amount of tokens pulled for tokenOut add liquidity
+        const amount2RemainingAfterSwap = amountInDesired.asFraction.subtract(amount2FromSwap.asFraction).quotient.toString()
+        const amount2Pulled = userBalancePrev2.sub(userBalanceCurrent2).toString()
+        expect(amount2RemainingAfterSwap).to.equal(amount2Pulled)
+  		})
+
+      it('potential events test', async () => {
+        const tx = await (await router['multicall(bytes[])']([methodParameters.calldata], { value: 0 })).wait()
+        // const token0_logs = tx?.logs?.map(event => { if (event.address === tokens[2].address) return tokens[2].interface.parseLog(event)})
+        // token0_logs.forEach((log) => {
+        //   log ? console.log(log) : console.log('')
+        //   log ? console.log(log.args.value.toString()) : console.log('')
+        //
+        // })
+        //
+        // console.log(wallet.address)
+      })
+    })
   })
 })
