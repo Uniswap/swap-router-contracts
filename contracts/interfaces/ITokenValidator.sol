34a888d264603fabb3ce9aeec1613b0d18523c89
Will Pote <will@uniswap.org>
2022-01-23 14:11:20 -0500

    FeeOnTransferLens to TokenValidation lens

--- contracts/interfaces/ITokenValidator.sol
+++ contracts/interfaces/ITokenValidator.sol
@@ -2,30 +2,36 @@
 pragma solidity >=0.7.5;
 pragma abicoder v2;
 
-/// @notice Determine if a token takes a fee on transfer by flash borrowing from the token/<base token> pool on V2.
-/// @notice Returns true if we detected a fee is taken on transfer.
-/// @notice A return value of false does not mean the token is definitely not a fee on transfer token. It means
-/// @notice we are *unsure* if the token will take a fee on transfer.
+/// @notice Validates tokens by flash borrowing from the token/<base token> pool on V2.
+/// @notice Returns
+///     Status.FOT if we detected a fee is taken on transfer.
+///     Status.STF if transfer failed for the token.
+///     Status.UNKN if we did not detect any issues with the token.
+/// @notice A return value of Status.UNKN does not mean the token is definitely not a fee on transfer token
+///     or definitely has no problems with its transfer. It just means we cant say for sure that it has any
+///     issues.
 /// @dev We can not guarantee the result of this lens is correct for a few reasons:
-/// 1/ Some tokens only take fees under specific conditions, for example some have an allowlist
-///     of addresses that do/dont require fees. Therefore the result is not guaranteed to be correct
-///     in all circumstances.
-/// 2/ It is possible that the token does not have any pools on V2 therefore we are not able to perform
-///     a flashloan to determine if the function would take a fee on transfer.
+/// @dev 1/ Some tokens take fees or allow transfers under specific conditions, for example some have an allowlist
+/// @dev    of addresses that do/dont require fees. Therefore the result is not guaranteed to be correct
+/// @dev    in all circumstances.
+/// @dev 2/ It is possible that the token does not have any pools on V2 therefore we are not able to perform
+/// @dev    a flashloan to test the token.
 /// @dev These functions are not marked view because they rely on calling non-view functions and reverting
 /// to compute the result.
-interface IFeeOnTransfer {
+interface ITokenValidator {
+    enum Status {UNKN, FOT, STF}
+
     /// @notice Returns whether a token is fee on transfer or not
     /// @param token The address of the token to check for fee on transfer
     /// @param baseTokens The addresses of the tokens to try pairing with
     /// token when looking for a pool to flash loan from.
     /// @param amountToBorrow The amount to try flash borrowing from the pools
     /// @return bool True if the token is fee on transfer
-    function isFeeOnTransfer(
+    function validate(
         address token,
         address[] calldata baseTokens,
         uint256 amountToBorrow
-    ) external returns (bool);
+    ) external returns (Status);
 
     /// @notice Returns whether each provided token is fee on transfer or not
     /// @param tokens The addresses of the tokens to check for fee on transfer
@@ -33,9 +39,9 @@ interface IFeeOnTransfer {
     /// token when looking for a pool to flash loan from.
     /// @param amountToBorrow The amount to try flash borrowing from the pools
     /// @return bool True if the token is fee on transfer
-    function batchIsFeeOnTransfer(
+    function batchValidate(
         address[] calldata tokens,
         address[] calldata baseTokens,
         uint256 amountToBorrow
-    ) external returns (bool[] memory);
+    ) external returns (Status[] memory);
 }
--- contracts/lens/TokenValidator.sol
+++ contracts/lens/TokenValidator.sol
@@ -8,55 +8,62 @@ import '@uniswap/v2-core/contracts/interfaces/IUniswapV2Callee.sol';
 import '@uniswap/v2-core/contracts/interfaces/IUniswapV2Pair.sol';
 import '../libraries/UniswapV2Library.sol';
 import '../interfaces/ISwapRouter02.sol';
-import '../interfaces/IFeeOnTransfer.sol';
+import '../interfaces/ITokenValidator.sol';
 import '../base/ImmutableState.sol';
 
-/// @notice Determine if a token takes a fee on transfer by flash borrowing from the token/<base token> pool on V2.
-/// @notice Returns true if we detected a fee is taken on transfer.
-/// @notice A return value of false does not mean the token is definitely not a fee on transfer token. It means
-/// @notice we are *unsure* if the token will take a fee on transfer.
+/// @notice Validates tokens by flash borrowing from the token/<base token> pool on V2.
+/// @notice Returns
+///     Status.FOT if we detected a fee is taken on transfer.
+///     Status.STF if transfer failed for the token.
+///     Status.UNKN if we did not detect any issues with the token.
+/// @notice A return value of Status.UNKN does not mean the token is definitely not a fee on transfer token
+///     or definitely has no problems with its transfer. It just means we cant say for sure that it has any
+///     issues.
 /// @dev We can not guarantee the result of this lens is correct for a few reasons:
-/// @dev 1/ Some tokens take fees under specific conditions, for example some have an allowlist
+/// @dev 1/ Some tokens take fees or allow transfers under specific conditions, for example some have an allowlist
 /// @dev    of addresses that do/dont require fees. Therefore the result is not guaranteed to be correct
 /// @dev    in all circumstances.
 /// @dev 2/ It is possible that the token does not have any pools on V2 therefore we are not able to perform
-/// @dev    a flashloan to test the transfer.
-contract FeeOnTransfer is IFeeOnTransfer, IUniswapV2Callee, ImmutableState {
+/// @dev    a flashloan to test the token.
+contract TokenValidator is ITokenValidator, IUniswapV2Callee, ImmutableState {
     string internal constant FOT_REVERT_STRING = 'FOT';
+    // https://github.com/Uniswap/v2-core/blob/1136544ac842ff48ae0b1b939701436598d74075/contracts/UniswapV2Pair.sol#L46
+    string internal constant STF_REVERT_STRING_SUFFIX = 'TRANSFER_FAILED';
 
     constructor(address _factoryV2, address _positionManager) ImmutableState(_factoryV2, _positionManager) {}
 
-    function batchIsFeeOnTransfer(
+    function batchValidate(
         address[] calldata tokens,
         address[] calldata baseTokens,
         uint256 amountToBorrow
-    ) public override returns (bool[] memory isFotResults) {
-        isFotResults = new bool[](tokens.length);
+    ) public override returns (Status[] memory isFotResults) {
+        isFotResults = new Status[](tokens.length);
         for (uint256 i = 0; i < tokens.length; i++) {
-            isFotResults[i] = isFeeOnTransfer(tokens[i], baseTokens, amountToBorrow);
+            isFotResults[i] = validate(tokens[i], baseTokens, amountToBorrow);
         }
     }
 
-    function isFeeOnTransfer(
+    function validate(
         address token,
         address[] calldata baseTokens,
         uint256 amountToBorrow
-    ) public override returns (bool) {
+    ) public override returns (Status) {
         for (uint256 i = 0; i < baseTokens.length; i++) {
-            if (_isFeeOnTransfer(token, baseTokens[i], amountToBorrow)) {
-                return true;
+            Status result = _validate(token, baseTokens[i], amountToBorrow);
+            if (result == Status.FOT || result == Status.STF) {
+                return result;
             }
         }
-        return false;
+        return Status.UNKN;
     }
 
-    function _isFeeOnTransfer(
+    function _validate(
         address token,
         address baseToken,
         uint256 amountToBorrow
-    ) internal returns (bool) {
+    ) internal returns (Status) {
         if (token == baseToken) {
-            return false;
+            return Status.UNKN;
         }
 
         address pairAddress = UniswapV2Library.pairFor(this.factoryV2(), token, baseToken);
@@ -66,7 +73,7 @@ contract FeeOnTransfer is IFeeOnTransfer, IUniswapV2Callee, ImmutableState {
         (, bytes memory returnData) = address(pairAddress).call(abi.encodeWithSelector(IUniswapV2Pair.token0.selector));
 
         if (returnData.length == 0) {
-            return false;
+            return Status.UNKN;
         }
 
         address token0Address = abi.decode(returnData, (address));
@@ -78,14 +85,50 @@ contract FeeOnTransfer is IFeeOnTransfer, IUniswapV2Callee, ImmutableState {
         uint256 balanceBeforeLoan = IERC20(token).balanceOf(address(this));
 
         IUniswapV2Pair pair = IUniswapV2Pair(pairAddress);
+
         try
             pair.swap(amount0Out, amount1Out, address(this), abi.encode(balanceBeforeLoan, amountToBorrow))
         {} catch Error(string memory reason) {
-            return keccak256(bytes(reason)) == keccak256(bytes(FOT_REVERT_STRING));
+            if (isFotFailed(reason)) {
+                return Status.FOT;
+            }
+
+            if (isTransferFailed(reason)) {
+                return Status.STF;
+            }
+
+            return Status.UNKN;
         }
 
         // Swap always reverts so should never reach.
         revert('Unexpected error');
+    }
+
+    function isFotFailed(string memory reason) internal pure returns (bool) {
+        return keccak256(bytes(reason)) == keccak256(bytes(FOT_REVERT_STRING));
+    }
+
+    function isTransferFailed(string memory reason) internal pure returns (bool) {
+        // We check the suffix of the revert string so we can support forks that
+        // may have modified the prefix.
+        string memory stf = STF_REVERT_STRING_SUFFIX;
+
+        uint256 reasonLength = bytes(reason).length;
+        uint256 suffixLength = bytes(stf).length;
+        if (reasonLength < suffixLength) {
+            return false;
+        }
+
+        uint256 ptr;
+        uint256 offset = 32 + reasonLength - suffixLength;
+        bool transferFailed;
+        assembly {
+            ptr := add(reason, offset)
+            let suffixPtr := add(stf, 32)
+            transferFailed := eq(keccak256(ptr, suffixLength), keccak256(suffixPtr, suffixLength))
+        }
+
+        return transferFailed;
     }
 
     function uniswapV2Call(
