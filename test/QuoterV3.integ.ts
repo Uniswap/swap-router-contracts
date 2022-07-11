10e4c091f9ef5e9ed4890e7c29848e678f241c2f
Eric Zhong <eric.zhong@uniswap.org>
2022-07-11 16:00:23 -0400

    simplify route variables in integ tests by removing fee

--- test/QuoterV3.integ.ts
+++ test/QuoterV3.integ.ts
@@ -22,19 +22,19 @@ const DAI_V2_UNI_V2_WETH = encodePath([DAI, UNI, WETH], [V2_FEE, V2_FEE])
 const USDC_V2_UNI_V2_WETH = encodePath([USDC, UNI, WETH], [V2_FEE, V2_FEE])
 
 /// @dev basic V3 routes
-const USDC_V3_500_USDT = encodePath([USDC, USDT], [FeeAmount.LOW])
-const UNI_V3_3000_WETH = encodePath([UNI, WETH], [FeeAmount.MEDIUM])
+const USDC_V3_USDT = encodePath([USDC, USDT], [FeeAmount.LOW])
+const UNI_V3_WETH = encodePath([UNI, WETH], [FeeAmount.MEDIUM])
 
 /// @dev stablecoin IL routes
-const USDT_V3_500_DAI_V2_USDC = encodePath([USDT, DAI, USDC], [FeeAmount.LOW, V2_FEE])
-const DAI_V3_100_USDC_V2_USDT = encodePath([DAI, USDC, USDT], [100, V2_FEE])
+const USDT_V3_DAI_V2_USDC = encodePath([USDT, DAI, USDC], [FeeAmount.LOW, V2_FEE])
+const DAI_V3_USDC_V2_USDT = encodePath([DAI, USDC, USDT], [100, V2_FEE])
 
 /// @dev erc20 IL routes
 // V3 - V2
-const UNI_V3_3000_WETH_V2_DAI = encodePath([UNI, WETH, DAI], [FeeAmount.MEDIUM, V2_FEE])
-const USDC_V3_3000_UNI_V2_WETH = encodePath([USDC, UNI, WETH], [FeeAmount.MEDIUM, V2_FEE])
+const UNI_V3_WETH_V2_DAI = encodePath([UNI, WETH, DAI], [FeeAmount.MEDIUM, V2_FEE])
+const USDC_V3_UNI_V2_WETH = encodePath([USDC, UNI, WETH], [FeeAmount.MEDIUM, V2_FEE])
 // V2 - V3
-const UNI_V2_WETH_V3_3000_DAI = encodePath([UNI, WETH, DAI], [V2_FEE, FeeAmount.MEDIUM])
+const UNI_V2_WETH_V3_DAI = encodePath([UNI, WETH, DAI], [V2_FEE, FeeAmount.MEDIUM])
 
 /// @dev complex IL routes
 // (use two V3 pools)
@@ -97,7 +97,7 @@ describe('QuoterV3 integration tests', function () {
     it('V3-V2 stablecoin path with 6 decimal in start of path', async () => {
       const { amountOut, v3SqrtPriceX96AfterList, v3InitializedTicksCrossedList } = await quoterV3.callStatic[
         'quoteExactInput(bytes,uint256)'
-      ](USDT_V3_500_DAI_V2_USDC, expandToNDecimals(10000, 6))
+      ](USDT_V3_DAI_V2_USDC, expandToNDecimals(10000, 6))
 
       expect(amountOut).eq(BigNumber.from('9966336832'))
       expect(v3SqrtPriceX96AfterList[0].eq(BigNumber.from('0x10c6727487c45717095f'))).to.be.true
@@ -106,7 +106,7 @@ describe('QuoterV3 integration tests', function () {
     it('V3-V2 stablecoin path with 6 decimal in middle of path', async () => {
       const { amountOut, v3SqrtPriceX96AfterList, v3InitializedTicksCrossedList } = await quoterV3.callStatic[
         'quoteExactInput(bytes,uint256)'
-      ](DAI_V3_100_USDC_V2_USDT, expandTo18Decimals(10000))
+      ](DAI_V3_USDC_V2_USDT, expandTo18Decimals(10000))
 
       expect(amountOut).eq(BigNumber.from('9959354898'))
       expect(v3SqrtPriceX96AfterList[0].eq(BigNumber.from('0x10c715093f77e3073634'))).to.be.true
@@ -135,7 +135,7 @@ describe('QuoterV3 integration tests', function () {
   it('quotes V3-V2 erc20s with mixed decimal scales correctly', async () => {
     const { amountOut, v3SqrtPriceX96AfterList, v3InitializedTicksCrossedList } = await quoterV3.callStatic[
       'quoteExactInput(bytes,uint256)'
-    ](USDC_V3_3000_UNI_V2_WETH, expandToNDecimals(10000, 6))
+    ](USDC_V3_UNI_V2_WETH, expandToNDecimals(10000, 6))
 
     expect(amountOut).eq(BigNumber.from('3801923847986895918')) // 3.801923847986895918
     expect(v3SqrtPriceX96AfterList[0].eq(BigNumber.from('0x3110863ba621ac3915fd'))).to.be.true
@@ -144,7 +144,7 @@ describe('QuoterV3 integration tests', function () {
   it('quotes V3-V2 correctly', async () => {
     const { amountOut, v3SqrtPriceX96AfterList, v3InitializedTicksCrossedList } = await quoterV3.callStatic[
       'quoteExactInput(bytes,uint256)'
-    ](UNI_V3_3000_WETH_V2_DAI, expandTo18Decimals(10000))
+    ](UNI_V3_WETH_V2_DAI, expandTo18Decimals(10000))
 
     expect(amountOut).eq(BigNumber.from('80675538331724434694636'))
     expect(v3SqrtPriceX96AfterList[0].eq(BigNumber.from('0x0e83f285cb58c4cca14fb78b'))).to.be.true
@@ -163,7 +163,7 @@ describe('QuoterV3 integration tests', function () {
   it('quotes V2-V3 correctly', async () => {
     const { amountOut, v3SqrtPriceX96AfterList, v3InitializedTicksCrossedList } = await quoterV3.callStatic[
       'quoteExactInput(bytes,uint256)'
-    ](UNI_V2_WETH_V3_3000_DAI, expandTo18Decimals(10000))
+    ](UNI_V2_WETH_V3_DAI, expandTo18Decimals(10000))
 
     expect(amountOut).eq(BigNumber.from('81108655328627859394525'))
     expect(v3SqrtPriceX96AfterList[1].eq(BigNumber.from('0x0518b75d40eb50192903493d'))).to.be.true
@@ -172,7 +172,7 @@ describe('QuoterV3 integration tests', function () {
   it('quotes only V3 correctly', async () => {
     const { amountOut, v3SqrtPriceX96AfterList, v3InitializedTicksCrossedList } = await quoterV3.callStatic[
       'quoteExactInput(bytes,uint256)'
-    ](UNI_V3_3000_WETH, expandTo18Decimals(10000))
+    ](UNI_V3_WETH, expandTo18Decimals(10000))
 
     expect(amountOut.eq(BigNumber.from('32215526370828998898'))).to.be.true
   })
