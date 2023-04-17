ebef5ab8f7f12027bbd968fe002bb04a56f1514f
AntonD3 <antonv.dyadyuk@gmail.com>
2023-04-18 00:02:04 +0100

    Formatting

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -3,7 +3,7 @@ import '@nomiclabs/hardhat-etherscan'
 import '@nomiclabs/hardhat-waffle'
 import 'hardhat-typechain'
 import 'hardhat-watcher'
-import "@matterlabs/hardhat-zksync-solc"
+import '@matterlabs/hardhat-zksync-solc'
 
 const DEFAULT_COMPILER_SETTINGS = {
   version: '0.7.6',
